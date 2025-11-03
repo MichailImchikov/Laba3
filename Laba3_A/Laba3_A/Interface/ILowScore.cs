4bc0ada19b713bbec404ea1320d2a1d9021eea6c
MichailImchikov <Coklexminer@gmail.com>
2025-11-03 19:16:12 +0300

    no message

---
+++ Laba3_A/Laba3_A/Interface/ILowScore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3_A.Interface
+{
+    public interface ILowScore
+    {
+        public float GetLowScore(NodeTree node);
+    }
+}
---
+++ Laba3_A/Laba3_A/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3_A
+{
+    public class NodeTree 
+    {
+        public List<NodeTree> NextNodes = new();
+        public DataTask Data;
+        public List<int> BakedData;
+        public List<int> OpenData;
+        public List<NodeTree> GetSheets()
+        {
+            var listNodes = new List<NodeTree>();
+            if (NextNodes.Count == 0) 
+            {
+                listNodes.Add(this);
+                return listNodes;
+            }
+            foreach (var node in NextNodes) 
+            {
+                listNodes.AddRange(node.GetSheets());
+            }
+            return listNodes;
+        }
+    }
+
+
+}
