58a411aadff8a3b489fa82ce6220dfb7bdcd5127
MichailImchikov <Coklexminer@gmail.com>
2025-11-03 21:07:44 +0300

    no message

--- Laba3_A/Laba3_A/SheetTree.cs
+++ Laba3_A/Laba3_A/SheetTree.cs
@@ -1,22 +1,7 @@
 ﻿
 public class SheetTree
 {
-    public List<SheetTree> NextNodes = new();
     public DataTask Data;
     public List<int> BakedData;
     public List<int> OpenData;
-    public List<SheetTree> GetSheets()
-    {
-        var listNodes = new List<SheetTree>();
-        if (NextNodes.Count == 0)
-        {
-            listNodes.Add(this);
-            return listNodes;
-        }
-        foreach (var node in NextNodes)
-        {
-            listNodes.AddRange(node.GetSheets());
-        }
-        return listNodes;
-    }
 }
